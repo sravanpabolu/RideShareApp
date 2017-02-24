f6675caa659e9904cea184a03abb0b6449f7b965
sravanpabolu <TCS@geethikakoneru.local>
2017-02-24 18:01:56 +0530

    UI Designs

---
+++ RideShareApp/RideShareApp/Classes/ViewControllers/MyRidesViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MyRidesViewController.swift
+//  RideShareApp
+//
+//  Created by TCS on 24/02/17.
+//  Copyright © 2017 Tcs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyRidesViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+}
