624255c06e2e50fab72d93e09a0e2994d2c0ca8b
Sravan <sravan@sravans-macbook-pro.local>
2017-02-26 23:34:28 +0530

    User Profile data saved in database.

---
+++ RideShareApp/RideShareApp/Classes/DataManager/DatabaseManager.swift
@@ -0,0 +1,44 @@
+//
+//  DatabaseManager.swift
+//  RideShareApp
+//
+//  Created by Sravan on 26/02/17.
+//  Copyright © 2017 Tcs. All rights reserved.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class DatabaseManager: NSObject {
+    var dbRef: FIRDatabaseReference!
+    
+    override init() {
+        super.init()
+        dbRef = FIRDatabase.database().reference()
+    }
+    
+    func saveUserProfileData(user: User) {
+        
+        let userData = [
+            "Email"         : user.userEmail as Any,
+            "Name"          : user.userName as Any,
+            "Gender"        : user.userGender as Any,
+            "Vehicle Owner" : user.userVehicle.isVehicleOwner as Any,
+            "Vehicle Number" : user.userVehicle.vehicleNumber as Any,
+            "Vehicle Model" : user.userVehicle.vehicleModel as Any,
+            "Vehicle Source" : user.userVehicle.vehicleSource as Any,
+            "Vehicle Destination" : user.userVehicle.vehicleDestination as Any
+        ] as [String : Any]
+        
+        dbRef.child("RideShareAppData").setValue(userData, withCompletionBlock: { (error: Error?, ref:FIRDatabaseReference!) in
+            print("User data Created")
+            
+            if let error = error {
+                print("Error: while creating data in DB : \(error.localizedDescription)")
+            }
+        }
+        )
+        
+        
+        }
+}
