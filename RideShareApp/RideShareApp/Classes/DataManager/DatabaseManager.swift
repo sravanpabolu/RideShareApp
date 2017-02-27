0a39d7e31abaab10deb168560886ba17e50f833a
Sravan <sravan@sravans-macbook-pro.local>
2017-02-27 19:52:00 +0530

    added user node

--- RideShareApp/RideShareApp/Classes/DataManager/DatabaseManager.swift
+++ RideShareApp/RideShareApp/Classes/DataManager/DatabaseManager.swift
@@ -12,6 +12,8 @@ import FirebaseDatabase
 class DatabaseManager: NSObject {
     var dbRef: FIRDatabaseReference!
     let rootNode: String = "RideShareAppData"
+    let userNode: String = "User"
+    
     override init() {
         super.init()
         dbRef = FIRDatabase.database().reference()
@@ -19,9 +21,13 @@ class DatabaseManager: NSObject {
     
     func saveUserProfileData(user: User) {
         
+        guard !(user.userEmail?.isEmpty)! else {
+            return
+        }
+        
         let userData = [
             "Email"         : user.userEmail as Any,
-            "Name"          : user.userName as Any,
+            "Given Name"    : user.userName as Any,
             "Gender"        : user.userGender as Any,
             "Vehicle Owner" : user.userVehicle.isVehicleOwner as Any,
             "Vehicle Number" : user.userVehicle.vehicleNumber as Any,
@@ -30,13 +36,34 @@ class DatabaseManager: NSObject {
             "Vehicle Destination" : user.userVehicle.vehicleDestination as Any
         ] as [String : Any]
         
-        dbRef.child(rootNode).setValue(userData, withCompletionBlock: { (error: Error?, ref:FIRDatabaseReference!) in
+        /*
+         Format : 
+         "User": {
+            "user_id_is_the_email": {
+            "Given_Name": "John Smith",
+            "Contact_number": 9988998898,
+            "Gender": "Male",
+            }
+         }
+         */
+        
+        
+        dbRef.child(rootNode).child(userNode).child(user.userEmail!).setValue(userData, withCompletionBlock: { (error: Error?, ref:FIRDatabaseReference!) in
             print("User data Created")
             
             if let error = error {
                 print("Error: while creating data in DB : \(error.localizedDescription)")
             }
         }
+
+        
+//        dbRef.child(rootNode).setValue(userData, withCompletionBlock: { (error: Error?, ref:FIRDatabaseReference!) in
+//            print("User data Created")
+//            
+//            if let error = error {
+//                print("Error: while creating data in DB : \(error.localizedDescription)")
+//            }
+//        }
         )
         
         
