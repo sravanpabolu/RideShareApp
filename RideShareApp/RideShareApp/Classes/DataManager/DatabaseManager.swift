2c7b83746bdd0a16439508f53c21736fbe3b6cf8
Sravan <sravan@sravans-macbook-pro.local>
2017-03-02 22:04:54 +0530

    Util Class, Modification of data manager class

--- RideShareApp/RideShareApp/Classes/DataManager/DatabaseManager.swift
+++ RideShareApp/RideShareApp/Classes/DataManager/DatabaseManager.swift
@@ -72,4 +72,90 @@ class DatabaseManager: NSObject {
         
         
         }
+    
+    
+    /*
+     
+     func saveUserProfileData(user: User) {
+     
+     guard !(user.userEmail?.isEmpty)! else {
+     return
+     }
+     
+     if let emailString = user.userEmail {
+     
+     let formattedEmail = Utils.sharedInstance.trimCharacters(sourceString: emailString)
+     
+     //            let owner   = user.userVehicle.isVehicleOwner as Any
+     //            let vNum    = user.userVehicle.vehicleNumber as Any,
+     //            "VehicleModel"  : user.userVehicle.vehicleModel as Any,
+     //            "VehicleSource" : user.userVehicle.vehicleSource as Any,
+     //            "VehicleDestination" : user.userVehicle.vehicleDestination  as Any
+     
+     let vehicleData =
+     
+     Dictionary(dictionaryLiteral:
+     "VehicleOwner"  , user.userVehicle.isVehicleOwner as Any,
+     "VehicleNumber" , user.userVehicle.vehicleNumber as Any,
+     "VehicleModel"  , user.userVehicle.vehicleModel as Any,
+     "VehicleSource" , user.userVehicle.vehicleSource as Any,
+     "VehicleDestination" , user.userVehicle.vehicleDestination  as Any
+     )
+     
+     //                Dictionary (
+     //                [
+     //                "VehicleOwner"  : user.userVehicle.isVehicleOwner as Any,
+     //                "VehicleNumber" : user.userVehicle.vehicleNumber as Any,
+     //                "VehicleModel"  : user.userVehicle.vehicleModel as Any,
+     //                "VehicleSource" : user.userVehicle.vehicleSource as Any,
+     //                "VehicleDestination" : user.userVehicle.vehicleDestination  as Any
+     //                ]
+     //            )
+     
+     //            let userData = Dictionary (
+     //                [
+     //                "UserID"        : formattedEmail,
+     //                "Email"         : emailString,
+     //                "GivenName"     : user.userName as Any,
+     //                "Gender"        : user.userGender as Any,
+     //                "ContactNumber" : user.userContactNumber as Any,
+     //                "VehicleDetails": vehicleData
+     //                ] as [String : Any]
+     //            )
+     
+     /*
+     Format :
+     "User": {
+     "user_id_is_the_email": {
+     "Given_Name": "John Smith",
+     "Contact_number": 9988998898,
+     "Gender": "Male",
+     "VehicleDetails":{
+     
+     }
+     }
+     }
+     */
+     
+     
+     
+     
+     
+     
+     
+     
+     //            dbRef.child(rootNode)
+     //                .child(userNode)
+     //                .child(formattedEmail)
+     //                .setValue(userData, withCompletionBlock:
+     //                    { (error: Error?, ref:FIRDatabaseReference!) in
+     //                        print("User data Created")
+     //
+     //                        if let error = error {
+     //                            print("Error: while creating data in DB : \(error.localizedDescription)")
+     //                        }
+     //                })
+     }
+     }
+     */
 }
