f530c3787e31d030c37f50acb0ff3fb054ee6a7a
Sravan <sravan@sravans-macbook-pro.local>
2017-03-03 21:32:59 +0530

    Saving User Data with ride history and vehicle data

--- RideShareApp/RideShareApp/Classes/Models/User.swift
+++ RideShareApp/RideShareApp/Classes/Models/User.swift
@@ -12,8 +12,8 @@ class User: NSObject {
     
     public var userName:String?
     public var userEmail:String?
-    public var userGender:String?
-    public var userContactNumber:IntMax?
+    public var userGender:String = "NA"
+    public var userContactNumber:IntMax = 9999999999
     
     public var userVehicle:Vehicle = Vehicle()
     public var userRides:Rides = Rides()
