7b1c09fb71fd15f6540ee055857664f8f000987b
Agasthya <agasthya@agasthya.local>
2017-03-08 16:48:22 +0530

    Autocomplete places

--- RideShareApp/RideShareApp/Classes/ViewControllers/BookRideViewController.swift
+++ RideShareApp/RideShareApp/Classes/ViewControllers/BookRideViewController.swift
@@ -8,15 +8,21 @@
 
 import Foundation
 import UIKit
+import GooglePlaces
 
-class BookRideViewController: BaseViewController {
-    @IBOutlet weak var txtSource: UITextField!
-    @IBOutlet weak var txtDestination: UITextField!
+class BookRideViewController: BaseViewController, GMSAutocompleteViewControllerDelegate {
+    
+    @IBOutlet weak var btnSource: UIButton!
+    @IBOutlet weak var btnDestination: UIButton!
     @IBOutlet weak var txtNumberOfSeats: UITextField!
     @IBOutlet weak var pickerBookingDate: UIDatePicker!
+    
+    var currentButton:UIButton?
     var bookedDate:Date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         pickerBookingDate.setValue(UIColor.white, forKey: "textColor");
 //        pickerBookingDate.minimumDate = Date()
         pickerBookingDate.addTarget(self, action: #selector(setBookingTime(pickerVal:)) , for: UIControlEvents.valueChanged)
@@ -48,7 +54,36 @@ class BookRideViewController: BaseViewController {
         vc.iNumberOfSeats = Int(txtNumberOfSeats.text!)
         vc.dtBookingDate = self.bookedDate
     }
+    
     override func performSegue(withIdentifier identifier: String, sender: Any?) {
         
+    }
+    
+    @IBAction func btnSourceTapped(sender: UIButton) {
+        currentButton = sender
+        
+        let autoCompleteController = GMSAutocompleteViewController()
+        autoCompleteController.delegate = self;
+        
+        _ = navigationController?.pushViewController(autoCompleteController, animated: false)
+    }
+    
+    //MARK: Autocomplete place delegate methods
+    public func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Selected Place: \(place.name)")
+        
+        currentButton?.titleLabel?.text = place.name
+        _ = navigationController?.popViewController(animated: true)
+    }
+    
+    public func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        print("Error while searching place : \(error.localizedDescription)")
+        _ = navigationController?.popViewController(animated: true)
+        showAlert(title: "Error", message: error.localizedDescription)
+    }
+    
+    public func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        print("Place Selection Cancelled")
+        _ = navigationController?.popViewController(animated: true)
     }
 }
